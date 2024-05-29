72f0e1e1344dbab0bd44dd91374337d12b2bcf4d
Gal Shimer <galshimer2@gmail.com>
2024-05-29 15:19:12 +0300

    400px

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-function onBallClick(){
+
+var ballHeight = 100
+var ballWidth = 100
+function onBallClick(ball) {
+    if (ballHeight === 400 ) {
+        ballHeight = 100
+        ballWidth = 100
+    } else {
+        ballHeight += 50
+        ballWidth += 50
+        ball.style.height = ballHeight + 'px'
+        ball.style.width = ballWidth + 'px'
+    }
 }
+
